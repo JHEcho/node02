4df1127a9baa480580b9e8fe0ffe1a7017bd110c
seirenz <lunartic93@gmail.com>
2019-06-13 15:38:12 +0900

    Revert "1-2"
    This reverts commit 561d54d05250d7328bbea6d43c199387747e77de.

--- src/components/InfoBanner/InfoBanner.js
+++ src/components/InfoBanner/InfoBanner.js
@@ -1,27 +1,10 @@
 import React, {component} from 'react';
-
 import './InfoBanner.scss';
 
-
-
 export default function InfoBannerComponent() {
-
   return (
-
     <div className="info-banner">
-
-     Mate Test
-
-      <div className="row grid-default-style">
-
-      <div className="col-6 background-image-contain"><h1>Text1</h1> <p>sub text1</p></div>
-
-      <div className="col-6 aaaaaaaa"><h1>Text2</h1> <p> sub text2</p></div>
-
-      </div>
-
+      (Info Banner)
     </div>
-
   );
-
 }
